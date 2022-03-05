dbe1eea3fa3eb970c11204e45620852b8a547af6
FroznAnna <64392141+FroznAnna@users.noreply.github.com>
2022-03-05 09:09:31 +0000

    Create hack.js

---
+++ hack.js
@@ -0,0 +1,26 @@
+alert('Thank you for using FroznAnna\'s Quizlet Gravity Hack\nWhen the answer appears in the textbox, press space then enter\nChange line 18 to fit the language you are working in [Default is Spanish]')
+words = {};
+for(i = 0; i < Quizlet.gravityModeData.terms.length; i++){
+	if(Quizlet.gravityModeData.terms[i].definition != null){
+		words[Quizlet.gravityModeData.terms[i].definition] = Quizlet.gravityModeData.terms[i].word;
+	}
+}
+let sort = {};
+for(let key in words){
+  sort[words[key]] = key;
+}
+words = sort
+
+let lastWord;
+setInterval(function() {
+  let inputBox = document.getElementsByClassName('GravityTypingPrompt-input js-keymaster-allow')[0]
+  try {
+    let term = document.getElementsByClassName('TermText notranslate lang-es')[0].outerText
+    if (term != lastWord) {
+      inputBox.value = words[term]
+      lastWord = term
+    }
+  } catch {
+
+  }
+}, 0);
